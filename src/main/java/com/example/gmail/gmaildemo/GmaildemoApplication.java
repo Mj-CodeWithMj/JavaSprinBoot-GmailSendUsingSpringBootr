a4b928c19386299dad620e99abf54ee4b055e3e7
murtuza <murtuzat78@gmail.com>
2024-06-05 22:50:46 +0530

    Done

---
+++ src/main/java/com/example/gmail/gmaildemo/GmaildemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.gmail.gmaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmaildemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmaildemoApplication.class, args);
+		System.out.println("Server Running............");
+	}
+
+}
---
+++ src/test/java/com/example/gmail/gmaildemo/GmaildemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmail.gmaildemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmaildemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
